4ec6e3cd7e1503ccb7b719e995adecde6782f117
Pablo <tackpablo@gmail.com>
2022-06-08 07:45:01 -0700

    Added solutions to week 3 question 10

--- Week 3 Non Linear Data Structures/10 maxDepthBT.js
+++ Week 3 Non Linear Data Structures/10 maxDepthBT.js
@@ -6,7 +6,17 @@
 
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
-var maxDepth = function (root) {};
+// uses Depth First Search (DFS) and recursion
+
+var maxDepth = function (root) {
+  // if the root is null or undefined (no nodes) return 0
+  if (root === undefined || root === null) {
+    return 0;
+  }
+  // otherwise return the max of the max depth of the left or right of the root
+  // add + 1 since the root itself does not count
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
 
 console.log(maxDepth([3, 9, 20, null, null, 15, 7])); // 3
 console.log(maxDepth([1, null, 2])); // 2
